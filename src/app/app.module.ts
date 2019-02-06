74078e6849bf550df69c71b355c261660b26da54
LewisMartin <Lewis_J_Martin@hotmail.co.uk>
2019-02-06 16:11:49 +0000

    Add ChartJS & ng2-charts packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material';
+import { HttpClientModule } from '@angular/common/http';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,7 +13,6 @@ import { BreakdownComponent } from './components/breakdown/breakdown.component';
 import { TrackerComponent } from './components/tracker/tracker.component';
 
 import { BirdwatchDataService } from './services/birdwatch-data.service';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    ChartsModule
   ],
   providers: [
     BirdwatchDataService
